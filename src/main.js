a27c9877e3e21e628b15af9fa2af6ba7c2e6f2d8
zuojianglong <3155481042@qq.com>
2019-09-02 20:00:59 +0800

    导入vant

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
+import Vant from 'vant'
+import 'vant/lib/index.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
-
+Vue.use(Vant)
 new Vue({
   router,
   store,
