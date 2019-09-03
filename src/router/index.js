22d86e7420fa5d7a2f32dd2ced481ea8c05bcc61
zuojianglong <3155481042@qq.com>
2019-09-03 14:22:31 +0800

    配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -4,19 +4,13 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: Home
-    // },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    // }
-  ]
+  routes: [{
+    path: '/login',
+    name: 'login',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+            import(/* webpackChunkName: "about" */ '../views/Login.vue')
+  }]
 })
