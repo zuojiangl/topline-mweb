1cf6a421dcc7771bbf65cc83dec7d2117377da3e
zuojianglong <3155481042@qq.com>
2019-09-03 12:00:37 +0800

    axios修改

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,21 +1,11 @@
 import axios from 'axios'
 import JSONbig from 'json-bigint'
 const request = axios.create({
-  baseURL: 'http://ttapi.research.itcast.cn/'
-})
-// 请求拦截器
-request.interceptors.request.use(function (config) {
-  return config
-}, function (error) {
-  return Promise.reject(error)
-})
-// 响应拦截器
-request.interceptors.response.use(function (response) {
-  return response
-}, function (error) {
-  return Promise.reject(error)
+  baseURL: 'http://ttapi.research.itcast.cn'
 })
 
+// 获取到服务器返回的数据,处理数据之前
+// transformRequest是在响应拦截器之前前执行
 request.defaults.transformRequest = [function (data) {
   try {
     // data数据可能不是标准的JSON格式字符串，否则会导致JSONbig.parse(data)转换失败报错
@@ -24,4 +14,19 @@ request.defaults.transformRequest = [function (data) {
     return data
   }
 }]
+
+// 请求拦截器
+request.interceptors.request.use(function (config) {
+  return config
+}, function (error) {
+  return Promise.reject(error)
+})
+// 响应拦截器
+request.interceptors.response.use(function (response) {
+  console.log(response)
+  return response
+}, function (error) {
+  return Promise.reject(error)
+})
+
 export default request
