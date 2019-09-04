c30016a6ca340e980c8c7a42fbe523ca4f66a576
zuojianglong <3155481042@qq.com>
2019-09-04 09:36:26 +0800

    首页布局1

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    home
+    <van-nav-bar title="首页"></van-nav-bar>
+    <van-tabs>
+    <van-tab v-for="index in 8" :key="index" :title="'标签 ' + index">
+     内容 {{ index }}
+    </van-tab>
+</van-tabs>
   </div>
 </template>
 
