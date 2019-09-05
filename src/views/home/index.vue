b594cfbba7dd03cd8f677a2a9ee3ec7472a112fa
zuojianglong <3155481042@qq.com>
2019-09-05 14:26:05 +0800

    首页布局list组件，深度作用选择器

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,20 +1,57 @@
 <template>
   <div>
-    <van-nav-bar title="首页"></van-nav-bar>
+    <van-nav-bar title="首页" fixed></van-nav-bar>
     <van-tabs>
-    <van-tab v-for="index in 8" :key="index" :title="'标签 ' + index">
-     内容 {{ index }}
-    </van-tab>
-</van-tabs>
+      <van-tab v-for="index in 8" :key="index" :title="'标签 ' + index">
+        <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
+          <van-cell v-for="item in list" :key="item" :title="item" />
+        </van-list>
+      </van-tab>
+    </van-tabs>
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      list: [],
+      loading: false,
+      finished: false
+    }
+  },
 
+  methods: {
+    onLoad () {
+      // 异步更新数据
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1)
+        }
+        // 加载状态结束
+        this.loading = false
+
+        // 数据全部加载完成
+        if (this.list.length >= 40) {
+          this.finished = true
+        }
+      }, 500)
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.van-tabs {
+  /deep/ .van-tabs__wrap {
+    position: fixed;
+    top: 46px;
+    left: 0;
+    z-index: 100;
+  }
+  /deep/ .van-tabs__content {
+    margin-top: 90px;
+    margin-bottom: 50px;
+  }
+}
 </style>
