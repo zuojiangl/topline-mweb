6a543846c8396ce457d082235e87d60119df1a69
zuojianglong <3155481042@qq.com>
2019-09-06 20:22:26 +0800

    弹层显示内容

--- src/views/home/components/MoreAction.vue
+++ src/views/home/components/MoreAction.vue
@@ -1,11 +1,20 @@
 <template>
   <van-dialog
   v-model="show"
-  title="标题"
   :show-confirm-button="false"
   close-on-click-overlay
 >
-hello
+  <van-cell-group v-show="!showReports">
+    <van-cell title="不感兴趣" icon="location-o" />
+    <van-cell title="反馈垃圾内容" icon="location-o" is-link @click="showReports= true"/>
+    <van-cell title="拉黑作者" icon="location-o" />
+  </van-cell-group>
+  <van-cell-group v-show="showReports">
+    <van-cell icon="arrow-left" @click="showReports= false"/>
+    <van-cell title="标题夸张"/>
+    <van-cell title="低俗色情"/>
+    <van-cell title="错别字多"/>
+  </van-cell-group>
 </van-dialog>
 </template>
 
@@ -13,7 +22,9 @@ hello
 export default {
   data () {
     return {
-      show: true
+      show: true,
+      // 控制举报组件显隐
+      showReports: false
     }
   }
 }
