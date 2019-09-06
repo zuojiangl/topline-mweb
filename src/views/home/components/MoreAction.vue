a1d922dc3a3d35946205bc8455d7252e60a362cf
zuojianglong <3155481042@qq.com>
2019-09-06 20:01:11 +0800

    dialog组件的使用

--- src/views/home/components/MoreAction.vue
+++ src/views/home/components/MoreAction.vue
@@ -1,12 +1,21 @@
 <template>
-  <div>
-    123
-  </div>
+  <van-dialog
+  v-model="show"
+  title="标题"
+  :show-confirm-button="false"
+  close-on-click-overlay
+>
+hello
+</van-dialog>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      show: true
+    }
+  }
 }
 </script>
 
