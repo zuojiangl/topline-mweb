813f1bbb3db8f587d7c4079b88eb8ebe6d5cc321
zuojianglong <3155481042@qq.com>
2019-09-07 22:54:45 +0800

    展示推荐频道

--- src/api/channel.js
+++ src/api/channel.js
@@ -8,3 +8,8 @@ import request from '@/utils/request'
 export const getDefaultOrUserChannels = () => {
   return request.get('/app/v1_0/user/channels')
 }
+
+// 获取全部频道
+export const getAllChannels = () => {
+  return request.get('/app/v1_0/channels')
+}
--- src/views/home/components/ChannelEdit.vue
+++ src/views/home/components/ChannelEdit.vue
@@ -33,13 +33,14 @@
     </van-grid>
     <van-cell title="单元格" value="内容" label="描述信息" />
     <van-grid>
-        <van-grid-item v-for="value in 8" :key="value" text="文字">
+        <van-grid-item v-for="channel in recommendChannels" :key="channel.id" :text="channel.name">
         </van-grid-item>
     </van-grid>
   </van-popup>
 </template>
 
 <script>
+import { getAllChannels } from '@/api/channel'
 export default {
   props: {
     value: {
@@ -51,10 +52,41 @@ export default {
       required: true
     }
   },
+  computed: {
+    recommendChannels () {
+      // 1.获取我的频道中所有id组成的数组
+      // map() 遍历数组，返回一个新数组，新数组中的元素由回调函数中返回的元素组成
+      const ids = this.channels.map((channel) => {
+        return channel.id
+      })
+      // 2.过滤所有频道，把频道id出现在上面数组中的项去除
+      // filter() 把满足条件的item，返回组成一个新的数组
+      return this.allChannels.filter((channel) => {
+        // includes() es6新增，判断数组中是否包含某一项
+        return !ids.includes(channel.id)
+      })
+    }
+  },
   data () {
     return {
       // 是否是编辑模式
-      isEdit: false
+      isEdit: false,
+      // 储存所有频道
+      allChannels: []
+    }
+  },
+  created () {
+    this.loadAllChannels()
+  },
+  methods: {
+    // 加载所有频道列表
+    async loadAllChannels () {
+      try {
+        const data = await getAllChannels()
+        this.allChannels = data.channels
+      } catch (err) {
+        console.log(err)
+      }
     }
   }
 }
