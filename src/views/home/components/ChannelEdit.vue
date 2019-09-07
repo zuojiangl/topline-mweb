e87a28859845bb9f5917e74242515f00375d628f
zuojianglong <3155481042@qq.com>
2019-09-07 22:34:03 +0800

    控制编辑模式

--- src/views/home/components/ChannelEdit.vue
+++ src/views/home/components/ChannelEdit.vue
@@ -8,11 +8,27 @@
     <van-cell icon="cross" @click="$emit('input',false)"></van-cell>
     <!-- 我的频道 -->
     <van-cell title="我的频道" label="点击进入频道">
-      <van-button round type="danger" size="mini">编辑</van-button>
+      <van-button
+        round
+        type="danger"
+        size="mini"
+        v-show="!isEdit"
+        @click="isEdit=true">
+        编辑
+        </van-button>
+      <van-button
+        round
+        type="danger"
+        size="mini"
+        v-show="isEdit"
+        @click="isEdit=false">
+        完成
+        </van-button>
     </van-cell>
     <van-grid>
         <van-grid-item v-for="value in 8" :key="value" text="文字">
-          <van-icon slot="icon" class="close-icon" name="close"></van-icon>
+          <!-- 关闭按钮 -->
+          <van-icon slot="icon" v-show="isEdit" class="close-icon" name="close"></van-icon>
         </van-grid-item>
     </van-grid>
     <van-cell title="单元格" value="内容" label="描述信息" />
@@ -32,7 +48,10 @@ export default {
     }
   },
   data () {
-    return {}
+    return {
+      // 是否是编辑模式
+      isEdit: false
+    }
   }
 }
 </script>
