f9fa6eb87894bd16974f1a17975c7544c79baa05
zuojianglong <3155481042@qq.com>
2019-09-07 17:35:58 +0800

    举报文章

--- src/views/home/components/MoreAction.vue
+++ src/views/home/components/MoreAction.vue
@@ -16,15 +16,13 @@ value是通过props单向传递的
   </van-cell-group>
   <van-cell-group v-show="showReports">
     <van-cell icon="arrow-left" @click="showReports= false"/>
-    <van-cell title="标题夸张"/>
-    <van-cell title="低俗色情"/>
-    <van-cell title="错别字多"/>
+    <van-cell v-for="item in reportList" :key="item.type" :title="item.title" @click="handle('report',item.type)"/>
   </van-cell-group>
 </van-dialog>
 </template>
 
 <script>
-import { dislikeArticle } from '@/api/article'
+import { dislikeArticle, reportArticle } from '@/api/article'
 import { blacklists } from '@/api/user'
 
 export default {
@@ -42,11 +40,22 @@ export default {
   data () {
     return {
       // 控制举报组件显隐
-      showReports: false
+      showReports: false,
+      reportList: [
+        { title: '标题夸张', type: 1 },
+        { title: '低俗色情', type: 2 },
+        { title: '错别字多', type: 3 },
+        { title: '旧闻重复', type: 4 },
+        { title: '广告软文', type: 5 },
+        { title: '内容不实', type: 6 },
+        { title: '涉嫌违法犯罪', type: 7 },
+        { title: '侵权', type: 8 },
+        { title: '其他问题', type: 0 }
+      ]
     }
   },
   methods: {
-    handle (type) {
+    handle (type, reportType) {
       switch (type) {
         case 'dislike':
           // 不感兴趣
@@ -55,6 +64,10 @@ export default {
         case 'blacklist':
           // 拉黑作者
           this.blacklistUser()
+          break
+        case 'report':
+          // 拉黑作者
+          this.report(reportType)
           break
       }
     },
@@ -78,6 +91,20 @@ export default {
         // 隐藏，移除掉数据
         // 告知父组件，操作成功
         this.$emit('handleSuccess')
+      } catch (err) {
+        this.$toast.fail('操作失败')
+      }
+    },
+    // 举报文章
+    async report (reportType) {
+      try {
+        await reportArticle({
+          target: this.article.art_id,
+          type: reportType
+        })
+        // 告诉父组件隐藏对话框
+        this.$emit('input', false)
+        this.$toast.success('操作成功')
       } catch (err) {
         this.$toast.fail('操作失败')
       }
