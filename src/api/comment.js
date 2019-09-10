bf9dfc2463e6255b1719053550905397c697c8fb
zuojianglong <3155481042@qq.com>
2019-09-10 10:02:31 +0800

    封装发布评论api

--- src/api/comment.js
+++ src/api/comment.js
@@ -1,5 +1,6 @@
 import request from '@/utils/request'
 
+// 获取评论列表
 export const getComments = ({
   // 评论类型，a-对文章(article)的评论，c-对评论(comment)的回复
   isArticle,
@@ -17,5 +18,22 @@ export const getComments = ({
       offset,
       limit
     }
+  })
+}
+
+// 发布评论
+// 评论的目标id（评论文章即为文章id，对评论进行回复则为评论id）
+// target,
+// 评论内容
+// content,
+// 文章id，对评论内容发表回复时，需要传递此参数，表明所属文章id。对文章进行评论，不要传此参数。
+// art_id
+export const sendComment = ({
+  obj
+}) => {
+  return request.post('/app/v1_0/comments', {
+    target: obj.target,
+    content: obj.content,
+    art_id: obj.art_id
   })
 }
