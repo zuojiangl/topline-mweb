11b9bcb57cbaef874986363e81e33dd778c5ca0b
zuojianglong <3155481042@qq.com>
2019-09-12 18:50:48 +0800

    上传图片-提示

--- src/views/user-profile/component/UploadFile.vue
+++ src/views/user-profile/component/UploadFile.vue
@@ -59,6 +59,14 @@ export default {
       this.$dialog.confirm({
         message: '是否确认该图片作为头像'
       }).then(async () => {
+        // 加载提示
+        this.$toast.loading({
+          duration: 0, // 持续展示toast
+          forbidClick: true, // 禁用北京点击
+          loadingType: 'spinner',
+          message: '正在上传...'
+
+        })
         try {
           const data = await uploadPhoto('photo', this.$refs.file.files[0])
           // 更改当前的头像
@@ -68,6 +76,8 @@ export default {
         } catch (err) {
           this.$toast.fail('头像上传失败')
         }
+        // 关闭加载提示
+        this.$toast.clear()
       })
     }
   }
