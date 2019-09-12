130d8dbb7d29d2f8579a5d772cea0b4adbb78c95
zuojianglong <3155481042@qq.com>
2019-09-12 15:24:03 +0800

    我的页面-显示未登录

--- src/views/Tabbar.vue
+++ src/views/Tabbar.vue
@@ -5,16 +5,20 @@
       <van-tabbar-item to="/" icon="home-o">首页</van-tabbar-item>
       <van-tabbar-item to="/qa" icon="search">问答</van-tabbar-item>
       <van-tabbar-item to="video" icon="friends-o">视频</van-tabbar-item>
-      <van-tabbar-item to="/user" icon="setting-o">我的</van-tabbar-item>
+      <van-tabbar-item to="/user" icon="setting-o">{{user ? '我的' : '未登录'}}</van-tabbar-item>
     </van-tabbar>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
   data () {
     return {
     }
+  },
+  computed: {
+    ...mapState(['user'])
   }
 }
 </script>
--- src/views/User.vue
+++ src/views/User.vue
@@ -1,13 +1,46 @@
 <template>
-  <div>user</div>
+  <div>
+    <div class="not-login">
+      <div class="circle" @click="handleLogin">
+        <span>登录</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  name: 'User',
+  methods: {
+    handleLogin () {
+      this.$router.push({
+        path: '/login',
+        query: {
+          redirect: this.$route.fullPath
+        }
+      })
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="less">
+.not-login {
+  height: 150px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .circle {
+    width: 50px;
+    height: 50px;
+    padding: 20px;
+    background-color: red;
+    border-radius: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    font-size: 20px;
+  }
+}
 </style>
