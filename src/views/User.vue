f7622d40c5eb54ad6905537e6760d6a749e3a5b7
zuojianglong <3155481042@qq.com>
2019-09-12 16:14:54 +0800

    我的页面-登录页面

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,16 +1,64 @@
 <template>
   <div>
-    <div class="not-login">
+    <!-- 未登录 -->
+    <div v-if="!user" class="not-login">
       <div class="circle" @click="handleLogin">
         <span>登录</span>
       </div>
+    </div>
+        <!-- 已登录 -->
+    <div v-else>
+      <van-cell-group class="user-info">
+        <van-cell class="base-info" is-link :border="false">
+          <div slot="title">
+            <img class="avatar" src="http://toutiao.meiduo.site/FgSTA3msGyxp5-Oufnm5c0kjVgW7" alt="">
+            <span class="title">只是为了好玩儿</span>
+          </div>
+        </van-cell>
+        <van-grid class="data-info" :border="false">
+          <van-grid-item>
+            <span class="count">1</span>
+            <span class="text">头条</span>
+          </van-grid-item>
+          <van-grid-item>
+            <span class="count">2</span>
+            <span class="text">关注</span>
+          </van-grid-item>
+          <van-grid-item>
+            <span class="count">3</span>
+            <span class="text">粉丝</span>
+          </van-grid-item>
+          <van-grid-item>
+            <span class="count">4</span>
+            <span class="text">获赞</span>
+          </van-grid-item>
+        </van-grid>
+      </van-cell-group>
+      <van-cell-group>
+        <van-grid clickable>
+          <van-grid-item icon="star" text="我的收藏"/>
+          <van-grid-item icon="chat" text="我的评论"/>
+          <van-grid-item icon="like" text="我的点赞"/>
+          <van-grid-item icon="browsing-history" text="浏览历史"/>
+        </van-grid>
+      </van-cell-group>
+      <van-cell-group>
+        <van-cell title="消息通知" is-link />
+        <van-cell title="用户反馈" is-link />
+        <van-cell title="小智同学" is-link />
+        <van-cell title="系统设置" is-link to="/settings" />
+      </van-cell-group>
     </div>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
   name: 'User',
+  computed: {
+    ...mapState(['user'])
+  },
   methods: {
     handleLogin () {
       this.$router.push({
@@ -41,6 +89,36 @@ export default {
     align-items: center;
     color: #fff;
     font-size: 20px;
+  }
+}
+.user-info {
+  .base-info {
+    display: flex;
+    align-items: center;
+    background-color: #0096fa;
+    div {
+      display: flex;
+      align-items: center
+    }
+    .avatar {
+      margin-right: 15px;
+      width: 50px;
+      border-radius: 100%;
+    }
+  }
+  .data-info {
+    .text {
+      font-size: 14px;
+    }
+    .count {
+      font-size: 12px;
+    }
+  }
+  /deep/ .van-cell__right-icon {
+    color: #fff
+  }
+  /deep/ .van-grid-item__content {
+    background-color: #0096fa;
   }
 }
 </style>
