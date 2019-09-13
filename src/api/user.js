8fb5e8f2de8e44fa71aa4c5aaf8ddecd0b8b42d5
zuojianglong <3155481042@qq.com>
2019-09-13 16:54:43 +0800

    封装获取用户关注/粉丝的api

--- src/api/user.js
+++ src/api/user.js
@@ -51,3 +51,31 @@ export const uploadPhoto = (key, file) => {
   formData.append(key, file)
   return request.patch('/app/v1_0/user/photo', formData)
 }
+
+// 获取关注用户列表
+export const getFollowings = ({
+  // 页数
+  page,
+  // 每页数量
+  perPage
+}) => {
+  return request.get('/app/v1_0/user/followings', {
+    params: {
+      page,
+      per_page: perPage
+    }
+  })
+}
+
+// 获取用户的粉丝列表
+export const getFollowers = ({
+  page,
+  perPage
+}) => {
+  return request.get('/app/v1_0/user/followers', {
+    params: {
+      page,
+      per_page: perPage
+    }
+  })
+}
