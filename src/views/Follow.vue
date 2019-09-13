ca14791f975da7adbecfb5107a9d99b81534cc45
zuojianglong <3155481042@qq.com>
2019-09-13 16:12:47 +0800

    关注/粉丝-切换

--- src/views/Follow.vue
+++ src/views/Follow.vue
@@ -7,7 +7,7 @@
       @click-left="$router.back()">
 
     </van-nav-bar>
-    <van-tabs v-model="active">
+    <van-tabs v-model="active" color="blue">
       <van-tab title="关注"></van-tab>
       <van-tab title="粉丝"></van-tab>
     </van-tabs>
@@ -20,6 +20,15 @@ export default {
   data () {
     return {
       active: 1
+    }
+  },
+  // keep-alive的钩子函数，当组件激活的时候执行
+  activated () {
+    const type = this.$route.query.type
+    if (type === '1') {
+      this.active = 0
+    } else {
+      this.active = 1
     }
   }
 }
